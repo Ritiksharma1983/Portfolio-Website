f37f2c1e10714c63e0becbd7de9b5176ed940a0e
himanshukumar-web <himanshukumar160077@gmail.com>
2026-03-28 02:01:19 +0530

    Fix incorrect mailto link in Contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,8 +10,8 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Email</h4>
             <p>
-              <a href="mailto:Ritiksharma6999@gmail.com" data-cursor="disable">
-                Ritiksharma6999@gmail.com
+              <a href="mailto:Ritiksharma0001@zohomail.in" data-cursor="disable">
+                Ritiksharma0001@zohomail.in
               </a>
             </p>
             <h4>Phone</h4>
